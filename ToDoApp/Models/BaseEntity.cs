d86615f0a519e060de5d560582eafffa0d1ea65d
unknown <vaqufresulzade@gmail.com>
2025-09-07 19:35:12 +0400

    update commit

--- ToDoApp/Models/BaseEntity.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ToDoApp.Models;
-
-public class BaseEntity
-{
-    public int Id { get; set; }
-    public DateTimeOffset CreatedAt { get; set; }
-    public DateTimeOffset UpdatedAt { get; set; }
-}
--- ToDoApp/obj/Debug/net8.0/ToDoApp.AssemblyInfo.cs
+++ ToDoApp/obj/Debug/net8.0/ToDoApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7e495948b882389986ef2d7429027f097a3d458")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
